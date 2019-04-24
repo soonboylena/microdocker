349deaa044ae1f8db494405af295477445fead25
sunbo <sunbo@kws-info.com>
2019-04-24 10:03:37 +0800

    sleuth-中途

---
+++ ELK.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+work_path=$(dirname $(readlink -f $0))
+echo "脚本路径：$work_path"
+
+sudo rm -rf /opt/micro/ELK/
+sudo mkdir -p /opt/micro/ELK/
+sudo mkdir -p /opt/micro/ELK/log
+sudo chown -R $USER /opt/micro
+sudo ln -s $work_path/ELK/config /opt/micro/ELK/config
+
