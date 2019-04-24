b754e73c6df1c9cf63f7a68806e1fffd8a5dc386
sunbo <sunbo@kws-info.com>
2019-04-24 14:14:26 +0800

    ELK

--- ELK.sh
+++ ELK.sh
@@ -3,9 +3,9 @@
 work_path=$(dirname $(readlink -f $0))
 echo "脚本路径：$work_path"
 
-sudo rm -rf /opt/micro/ELK/
+#sudo rm -rf /opt/micro/ELK/
 sudo mkdir -p /opt/micro/ELK/
 sudo mkdir -p /opt/micro/ELK/log
 sudo chown -R $USER /opt/micro
-sudo ln -s $work_path/ELK/config /opt/micro/ELK/config
+sudo ln -sdf $work_path/ELK/config /opt/micro/ELK/config
 
